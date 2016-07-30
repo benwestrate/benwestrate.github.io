e647dabbc43a88f0be6b967f15ca384088df3ec9
Westrate, Benjamin <Benjamin.Westrate@icfi.com>
2016-07-30 15:16:52 -0500

    additions and clean up

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,38 @@
 $(window).ready( function(){
-    
+
     adjustPostitions();
-    
-    $(window).resize( adjustPostitions );
-    
+
+    window.onresize = adjustPostitions;
+
     function adjustPostitions(){
+
         if( $(window).width() > 992 ){
-            
-            $('.index .full-width').height( $(window).height() );
+
             $('[data-hook="summary"]').css( "margin-top" , ( $('.full-width').height() - 250 ) / 2 );
-            
+
+        } else {
+            $('[data-hook="summary"]').css( "margin-top" , ( 0 ) );
         }
     }
-    
+
+    var cards = document.querySelectorAll('.project-cards__card')
+    var activeCard = null;
+
+    cards.forEach( function( card ) {
+
+        card.onclick = function( e ) {
+            if( activeCard === card ){
+                activeCard.classList.remove('project-cards__card--active');
+            } else {
+                if( activeCard ){
+                    activeCard.classList.remove('project-cards__card--active');
+                }
+                card.classList.add('project-cards__card--active');
+            }
+
+            activeCard = card;
+        }
+
+    } )
+
 } );
